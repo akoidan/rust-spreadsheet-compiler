e32c091beaa6234bd021264620daa9488240f02c
akoidan <deathangel908@gmail.com>
2023-07-23 22:43:34 +0200

    check

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,22 @@ mod tests {
     #[test]
     fn parses_single_forumla() {
         let a = construct_table();
-        a.parse_string(String::from("=text(bte(@adjusted_cost<1>, @cost_threshold<1>))"), 16, String::from("cost_too_high"));
+        let value = a.parse_string(String::from("=text(bte(@adjusted_cost<1>, @cost_threshold<1>))"), 16, String::from("cost_too_high"));
+        println!("{}", value);
     }
 
     #[test]
     fn parses_single_forumla_2() {
         let a = construct_table();
-        a.parse_string(String::from("=concat(\"t_\", text(incFrom(1)))"), 2, String::from("transaction_id"));
+        let value = a.parse_string(String::from("=concat(\"t_\", text(incFrom(1)))"), 10, String::from("transaction_id"));
+        println!("{}", value);
+    }
+
+    #[test]
+    fn parses_single_forumla_3() {
+        let a = construct_table();
+        let value = a.parse_string(String::from("=E^v+(E^v*A9)"), 7, String::from("adjusted_cost"));
+        println!("{}", value);
     }
 
 }
--- src/table_factory.rs
+++ src/table_factory.rs
@@ -6,30 +6,32 @@ fn is_head(col: &str) -> bool {
 }
 
 pub fn lines_to_table(csv_str: &str) -> TableData {
-    let data: Vec<&str> = csv_str.split_to_vect('\n');
+    let rows: Vec<&str> = csv_str.split_to_vect('\n');
     let mut table: TableData = TableData { columns: vec![] };
     let mut current_head: Vec<String> = vec![];
 
-    for row_index in 0..data.len() {
-        let columns = data[row_index].split_to_vect('|');
-        let current_is_head: bool = is_head(columns[0]);
+    for row_index in 0..rows.len() {
+        let columns = rows[row_index].split_to_vect('|');
+        let current_is_head: bool = is_head(columns[0].trim());
         if current_is_head {
             current_head.clear();
             for column_index in 0..columns.len() {
-                if columns[column_index] == "" {
+                let column = columns[column_index].trim();
+                if column == "" {
                     continue;
                 }
-                assert!(is_head(columns[column_index]), "Invalid head at index {}", row_index);
-                current_head.push(String::from(columns[column_index].remove_first_symbol()));
-                table.add_column(columns[column_index].remove_first_symbol(), column_index);
+                assert!(is_head(column), "Invalid head at index {}", row_index);
+                current_head.push(String::from(column.remove_first_symbol()));
+                table.add_column(column.remove_first_symbol(), column_index);
             }
         } else {
             for column_index in 0..columns.len() {
-                if columns[column_index] == "" {
+                let column = columns[column_index].trim();
+                if column == "" {
                     continue;
                 }
                 let x = table.get_by_name(&current_head[column_index]);
-                x.values.insert(row_index as u32 +1, String::from(columns[column_index]));
+                x.values.insert(row_index as u32 +1, String::from(column));
             }
         }
     }
