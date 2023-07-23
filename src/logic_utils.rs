aaeef018d08e6ec41a3b4108a2480f654d0fe8b3
akoidan <deathangel908@gmail.com>
2023-07-23 22:07:46 +0200

    check

--- src/logic_utils.rs
+++ src/logic_utils.rs
@@ -21,6 +21,22 @@ impl Item {
         }
     }
 
+    fn get_end_zone_character(&self) -> char {
+        if let Item::ZoneEnd(s) = self {
+            *s
+        } else {
+            panic!("Current item should be a literal");
+        }
+    }
+
+    fn get_as_operator(&self) -> char {
+        if let Item::Operator(s) = self {
+            *s
+        } else {
+            panic!("Current item should be a literal");
+        }
+    }
+
     fn get_literal_as_text(&self) -> &str {
         if let Item::Literal(s) = self {
             return s.as_str();
@@ -53,11 +69,12 @@ pub trait LogicExecutor {
     fn fill_data(&mut self);
     fn revaluate_from_end_zone(&self, stack: &mut VecDeque<Item>);
     fn calc_function(&self, name: &str, args: &[String]) -> String;
+    fn evaluate_arithmetic(&self, operator: char, args: &[String]) -> String;
     fn resolve_literal_at(&self, s: &str, i: usize) -> (String, usize);
     fn revaluate_from_literal(&self, stack: &mut VecDeque<Item>);
-    fn get_matching_start_zone(item: Item) -> char;
     fn increase_column_digits(text: String, prev_num: u32) -> String;
     fn evaluate_column_reference(&self, stack: &mut VecDeque<Item>) -> Item;
+    fn evaluate_curly_zone(&self, stack: &mut VecDeque<Item>) -> Item;
 }
 
 impl LogicExecutor for TableData {
@@ -76,18 +93,6 @@ impl LogicExecutor for TableData {
             .replace_all(&text, |caps: &regex::Captures| {
                 caps[0][0..=0].to_string() + &(prev_num + 1).to_string()
             }).to_string();
-    }
-
-    fn get_matching_start_zone(item: Item) -> char {
-        let start_zone_value = match item {
-            Item::ZoneEnd(c) => match c {
-                '>' => '<',
-                ')' => '(',
-                _ => panic!("Invalid zone_end char"),
-            },
-            _ => panic!("Expected zone_end"),
-        };
-        return start_zone_value;
     }
 
     fn revaluate_from_literal(&self, stack: &mut VecDeque<Item>) {}
@@ -110,74 +115,84 @@ impl LogicExecutor for TableData {
         return Item::Literal(res);
     }
 
-    fn revaluate_from_end_zone(&self, stack: &mut VecDeque<Item>) {
-        let item = stack.pop_back().unwrap();
-        let mut operands: Vec<String> = Vec::new();
-        match item {
-            Item::Literal(val) => {
-                operands.push(val);
-            }
-            Item::ZoneEnd(val) if val == '>' => {
-                let column_res = self.evaluate_column_reference(stack);
-                stack.push_back(column_res);
-                return;
-            }
-            Item::ZoneEnd(val)  if val == '>' => {
-                let start_zone_value = TableData::get_matching_start_zone(item);
-                loop {
-                    let item_inner = stack.pop_back().expect("WTF");
-                    match item_inner {
-                        Item::Literal(val) => operands.push(val),
+    fn evaluate_curly_zone(&self, stack: &mut VecDeque<Item>) -> Item {
+        let mut operands: Vec<String> = vec![];
+        loop {
+            let item_inner = stack.pop_back().expect("No matching pair of ')' found");
+            if let Item::Literal(value) = item_inner {
+                operands.push(value);
+            } else if let Item::ZoneStart(value) = item_inner {
+                assert_eq!(value, '(', "no opening braces");
+                assert_eq!(operands.len(), 0, "Uncalculated expression on the start");
+                let name = stack
+                    .pop_back()
+                    .expect("function name")
+                    .get_token();
+                let res = self.calc_function(&name, &operands);
+                stack.push_back(Item::Literal(res));
 
-                        Item::ZoneStart(c) if c == start_zone_value => {
-                            if let Some(name_item) = stack.pop_back() {
-                                match name_item {
-                                    Item::Token(name) => {
-                                        let res = self.calc_function(&name, &operands);
-                                        stack.push_back(Item::Literal(res));
-                                    }
-                                    Item::Operator(op) => {
-                                        if let Some(name_2_item) = stack.pop_back() {
-                                            match name_2_item {
-                                                Item::Literal(val) => {
-                                                    operands.push(val);
-                                                    let res = self
-                                                        .calc_function(&op.to_string(), &operands);
-                                                    stack.push_back(Item::Literal(res));
-                                                    operands.clear();
-                                                }
-                                                _ => panic!("WTF"),
-                                            }
-                                        }
-                                    }
-                                    _ => panic!("WTF"),
-                                }
-                            }
-                            break;
-                        }
-                        Item::Operator(op) => {
-                            if let Some(name_item) = stack.pop_back() {
-                                match name_item {
-                                    Item::Literal(val) => {
-                                        operands.push(val);
-                                        let res = self.calc_function(&op.to_string(), &operands);
-                                        stack.push_back(Item::Literal(res));
-                                        operands.clear();
-                                    }
-                                    _ => panic!("WTF"),
-                                }
-                            }
-                        }
-                        _ => panic!("WTF"),
-                    }
-                }
+                // if let Some(name_item) = stack.pop_back() {
+                //     match name_item {
+                //         Item::Token(name) => {
+                //         }
+                //         Item::Operator(op) => {
+                //             if let Some(name_2_item) = stack.pop_back() {
+                //                 match name_2_item {
+                //                     Item::Literal(val) => {
+                //                         operands.push(val);
+                //                         let res = self
+                //                             .calc_function(&op.to_string(), &operands);
+                //                         stack.push_back(Item::Literal(res));
+                //                         operands.clear();
+                //                     }
+                //                     _ => panic!("WTF"),
+                //                 }
+                //             }
+                //         }
+                //         _ => panic!("WTF"),
+                //     }
+                // }
+            // part of arithmetic operations, validate the last one,
+            // and queue back to the stack in case there are multiple of them
+            // e.g. 2+3+4+6
+            } else if let Item::Operator(operator) = item_inner {
+                let left_operand = stack
+                    .pop_back()
+                    .expect("No left expression to operator")
+                    .get_literal_as_text().to_string();
+                operands.push(left_operand.to_string());
+                let res = self.evaluate_arithmetic(operator, &operands);
+                stack.push_back(Item::Literal(res));
+                operands.clear();
+            } else {
+                panic!("Invalid operation")
             }
-            _ => panic!("Unsupported type of expression"),
+        }
+    }
+
+    fn revaluate_from_end_zone(&self, stack: &mut VecDeque<Item>) {
+        let end_zone_symbol = stack
+            .pop_back()
+            .unwrap()
+            .get_end_zone_character();
+        // @adjusted_cost<1>
+        if end_zone_symbol == '<' {
+            let column_res = self.evaluate_column_reference(stack);
+            stack.push_back(column_res);
+            // split(D2, ",")
+            // (E^v*A9)
+        } else {
+            let column_res = self.evaluate_curly_zone(stack);
+            stack.push_back(column_res);
         }
     }
 
     fn calc_function(&self, name: &str, args: &[String]) -> String {
         format!("[{}({})]", name, args.join(","))
+    }
+
+    fn evaluate_arithmetic(&self, operator: char, args: &[String]) -> String {
+        format!("[{}({})]", operator, args.join(","))
     }
 
     fn resolve_literal_at(&self, s: &str, i: usize) -> (String, usize) {
@@ -203,42 +218,42 @@ impl LogicExecutor for TableData {
                 let (literal, literal_length) = self.resolve_literal_at(s, i);
                 i += literal_length;
                 stack.push_back(Item::Literal(literal))
-            // if token
+                // if token
             } else if s.at(i).is_ascii_alphabetic() {
-                let token_length = s.next_word_length(i+1);
+                let token_length = s.next_word_length(i + 1);
                 let token = s[i..i + token_length + 1].to_string();
                 stack.push_back(Item::Token(token));
                 i += token_length + 1;
-            // if number
+                // if number
             } else if s.at(i).is_ascii_digit() {
-                let digit_length = s.next_digit_length(i+1);
+                let digit_length = s.next_digit_length(i + 1);
                 let digit = &s[i..i + digit_length + 1];
                 stack.push_back(Item::Literal(digit.to_string()));
                 i += digit_length + 1;
-            // if expression starts
+                // if expression starts
             } else if ['(', '<'].contains(&s.at(i)) {
                 stack.push_back(Item::ZoneStart(s.at(i)));
                 i += 1;
-            // if expression end, here we need to evaluated all inside of it.
+                // if expression end, here we need to evaluated all inside of it.
             } else if [')', '>'].contains(&s.at(i)) {
                 stack.push_back(Item::ZoneEnd(s.at(i)));
                 i += 1;
                 self.revaluate_from_end_zone(&mut stack);
-            // if string literal
+                // if string literal
             } else if s.at(i) == '\"' {
-                let literal_length =  s.next_quote_length(i+1);
+                let literal_length = s.next_quote_length(i + 1);
                 let val = &s[i..i + literal_length + 2]; // 2 is start+end quote
                 stack.push_back(Item::Literal(val.to_string()));
                 i += literal_length + 2; // "asd" = 3 + 2
-            // if column reference
+                // if column reference
             } else if s.at(i) == '@' {
                 let token_length = s.next_word_length_underscore(i + 1);
                 stack.push_back(Item::Token(s[i..i + token_length + 1].to_string()));
                 i += token_length + 1; // @ + word itself
-            // ignore separators
+                // ignore separators
             } else if [' ', ','].contains(&s.at(i)) {
                 i += 1;
-            // arithmetic operations
+                // arithmetic operations
             } else if ['+', '*', '-', '/'].contains(&s.at(i)) {
                 stack.push_back(Item::Operator(s.at(i)));
                 i += 1;
